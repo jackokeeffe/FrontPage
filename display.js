cf7d9ff0beeca3a6089bd4fd42687cf6ecc79e93
Jack O'Keeffe <55100016+jackokeeffe@users.noreply.github.com>
2021-06-15 12:25:49 -0400

    Initial Commit: Frontend

---
+++ display.js
@@ -0,0 +1,36 @@
+// Used to fetch information from API
+async function get_data(){
+  var data = await fetch('https://this-front-page.herokuapp.com/news/');
+  update_elements([await data.json()]);
+}
+
+// Used to update the page with information from API
+function update_elements(data){
+  sources = ["nyt", "techCrunch", "cnet"]
+  classes = ["title", "description", "author", "source", "image", "link"]
+
+  var x = 0;
+  var y = 0;
+  var sourcesLength = sources.length - 1;
+  var classesLength = classes.length - 1;
+// Iterates through elements and changes then based on id/class name
+  while (y <= sourcesLength) {
+    var id = sources[y];
+    while (x <= classesLength) {
+    var targetData = data[0][sources[y]][classes[x]]
+      if (classes[x] == "image"){
+        document.getElementById(id).getElementsByClassName(classes[x])[0].src = targetData;
+        document.getElementById(id).getElementsByClassName(classes[x])[0].alt = data[0][sources[y]]["alt"];
+      } else if (classes[x] == "link"){
+        document.getElementById(id).getElementsByClassName(classes[x])[0].href = targetData;
+      } else {
+        document.getElementById(id).getElementsByClassName(classes[x])[0].innerHTML = targetData;
+      }
+      x++
+    }
+    x = 0
+    y++
+  }
+}
+
+get_data();
