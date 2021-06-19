3a0f895f8553f8fc201fdba5aec3f78890610146
Jack O'Keeffe <55100016+jackokeeffe@users.noreply.github.com>
2021-06-19 00:42:56 -0400

    Fixed Class Issue
    Fixed issue that stopped information from NYT from loading. This was due to a change in the classes used by the website.

--- nyt.py
+++ nyt.py
@@ -7,18 +7,14 @@ def times():
 	html = page.read().decode("utf-8")
 	soup = BeautifulSoup(html, "html.parser")
 	try:
-		main = str(soup.find('h2', {'class': 'css-4tvmog eecsw3u0'})).split('href="')[1].split('">')
-		directory = main[0]
-		title = main[1].split('</a>')[0]
-		description = str(soup.find('p', {'class': 'css-1jhf0lz eecsw3u1'})).split('>')[1].split('<')[0]
-		image = str(soup.find('article', {'class': 'css-1pagwdx'})).split('src="')[1].split('"')[0]
-		image_alt = str(soup.find('article', {'class': 'css-1pagwdx'})).split('<img alt="')[1].split('"')[0]
-		author = str(soup.find('p', {'class': 'css-s7sl7h e4e4i5l2'}))
-		soup = BeautifulSoup(author, "html.parser")
-		for data in soup(['style', 'script']):
-			data.decompose()
-		author = ''.join(soup.stripped_strings).split('By')[1].split('and')
-		author = author[0] + " and " + author[1]
+		main = str(soup.find('ol', {'class': 'css-11jjg ekkqrpp2'}))
+		directory = main.split('href="')[1].split('">')[0]
+		index = main.split('</a></div>')[1].split('.html">')[1]
+		title = index.split('<')[0]
+		description = index.split('">')[1].split('<')[0]
+		image = main.split('src="')[1].split('"')[0]
+		image_alt = main.split('img alt="')[1].split('src')[0]
+		author = index.split('itemprop')[1].split('>')[1].split('<')[0]
 		data = {'title': title, 'description': description, 'author': 'Author: ' + author, 'image': image, 'alt': image_alt, 'link': "https://www.nytimes.com/" + directory, "source": "New York Times"}
 		return data
 	except:
