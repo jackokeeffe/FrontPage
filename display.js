abe6e7f1104d64110f54e07c9bdc7658cb0a29ff
Jack O'Keeffe <55100016+jackokeeffe@users.noreply.github.com>
2021-06-21 18:09:27 -0400

    Updated display.js
    Simplified the steps to change the URL if running locally.

--- display.js
+++ display.js
@@ -1,36 +1,40 @@
+// Change this URL if running locally
+// It should look something like this: http://127.0.0.1:5000/news/
+var api_url = 'https://this-front-page.herokuapp.com/news/'
+
 // Used to fetch information from API
-async function get_data(){
-  var data = await fetch('https://this-front-page.herokuapp.com/news/');
-  update_elements([await data.json()]);
+async function get_data() {
+	var data = await fetch(api_url);
+	update_elements([await data.json()]);
 }
 
 // Used to update the page with information from API
-function update_elements(data){
-  sources = ["nyt", "techCrunch", "cnet"]
-  classes = ["title", "description", "author", "source", "image", "link"]
+function update_elements(data) {
+	sources = ["nyt", "techCrunch", "cnet"]
+	classes = ["title", "description", "author", "source", "image", "link"]
 
-  var x = 0;
-  var y = 0;
-  var sourcesLength = sources.length - 1;
-  var classesLength = classes.length - 1;
-// Iterates through elements and changes then based on id/class name
-  while (y <= sourcesLength) {
-    var id = sources[y];
-    while (x <= classesLength) {
-    var targetData = data[0][sources[y]][classes[x]]
-      if (classes[x] == "image"){
-        document.getElementById(id).getElementsByClassName(classes[x])[0].src = targetData;
-        document.getElementById(id).getElementsByClassName(classes[x])[0].alt = data[0][sources[y]]["alt"];
-      } else if (classes[x] == "link"){
-        document.getElementById(id).getElementsByClassName(classes[x])[0].href = targetData;
-      } else {
-        document.getElementById(id).getElementsByClassName(classes[x])[0].innerHTML = targetData;
-      }
-      x++
-    }
-    x = 0
-    y++
-  }
+	var x = 0;
+	var y = 0;
+	var sourcesLength = sources.length - 1;
+	var classesLength = classes.length - 1;
+	// Iterates through elements and changes then based on id/class name
+	while (y <= sourcesLength) {
+		var id = sources[y];
+		while (x <= classesLength) {
+			var targetData = data[0][sources[y]][classes[x]]
+			if (classes[x] == "image") {
+				document.getElementById(id).getElementsByClassName(classes[x])[0].src = targetData;
+				document.getElementById(id).getElementsByClassName(classes[x])[0].alt = data[0][sources[y]]["alt"];
+			} else if (classes[x] == "link") {
+				document.getElementById(id).getElementsByClassName(classes[x])[0].href = targetData;
+			} else {
+				document.getElementById(id).getElementsByClassName(classes[x])[0].innerHTML = targetData;
+			}
+			x++
+		}
+		x = 0
+		y++
+	}
 }
 
 get_data();
